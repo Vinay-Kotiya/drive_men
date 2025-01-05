5cda745853246246210c5055659c92f8d77213da
Vinay Kotiya <vinaykotiya77@gmail.com>
2025-01-06 00:12:49 +0530

    last commit

--- app.js
+++ app.js
@@ -16,23 +16,23 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //cors configuration
-app.use(cors());
-// const allowedOrigins =
-//   process.env.NODE_ENV === "production"
-//     ? [
-//         `${process.env.FRONT_END_URL}`,
-//         "https://drivecloneapi.vercel.app",
-//         "https://driveclonemern.vercel.app",
-//       ]
-//     : ["http://localhost:5173"]; // Allow localhost in development
+// app.use(cors());
+const allowedOrigins =
+  process.env.NODE_ENV === "production"
+    ? [
+        `${process.env.FRONT_END_URL}`,
+        "https://drivecloneapi.vercel.app",
+        "https://driveclonemern.vercel.app",
+      ]
+    : ["http://localhost:5173"]; // Allow localhost in development
 
-// app.use(
-//   cors({
-//     origin: allowedOrigins,
-//     methods: ["POST", "GET", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Handle preflight requests
 app.options("*", cors()); // This line handles OPTIONS requests globally
